737d53bac3e9a8bc8bbc3b47556a2dd89199381b
Garima Chandra <garich01@noa.nintendo.com>
2016-11-09 10:34:30 -0800

    initial commit for new test case

--- src/test/java/com/rest/city/PositiveTestZipCode.java
+++ src/test/java/com/rest/city/PositiveTestZipCode.java
@@ -3,9 +3,7 @@ package com.rest.city;
 import groovy.util.logging.Slf4j;
 import org.testng.annotations.Test;
 
-/**
- * Created by garich01 on 11/7/2016.
- */
+
 @Slf4j
 public class PositiveTestZipCode {
 
